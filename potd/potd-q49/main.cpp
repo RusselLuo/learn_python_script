37baae3d03e8138d6641879aa2db32aec4e947da
RusselLuo <russelluo@gmail.com>
2017-04-12 10:23:03 -0500

    a

---
+++ potd/potd-q49/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+vector<int> checkSums(vector<int> v){
+
+
+	//write your code here
+
+	return sums;
+}
+
+
+int main(){
+
+vector<int> v1;
+
+v1 = {20,32,12,65,0,19,76,15,2,39};
+
+vector<int>answer = checkSums(v1);
+
+cout << "The sum of the sum of the largest two elements is " << answer[0] <<endl;
+cout << "The sum of the sum of the smallest two elements is " << answer[1] <<endl;
+
+return 0;
+
+}
