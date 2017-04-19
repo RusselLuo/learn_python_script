64dca19f99bf81ac4037a94db5f6ac720c907e23
RusselLuo <russelluo@gmail.com>
2017-04-19 01:34:49 -0500

    potd53

---
+++ potd/potd-q53/mystery.cpp
@@ -0,0 +1,28 @@
+#include "functions.h"
+
+void mystery() {
+  // There are a number of functions defined in "functions.h" and
+  // implemented in "functions.cpp". Your job is to call those
+  // functions with the correct arguments so that the Alma Mater lyrics
+  // are printed when this function is called.
+
+  // If you're successful, this is the output you'll see:
+
+  // Hail to the Orange.
+  // Hail to the Blue.
+  // Hail Alma Mater,
+  // Ever so true.
+  // We love no other,
+  // So let our motto be
+  // Victory, Illinois, Varsity.
+
+  // Your code here!
+  function1(1);
+  function2((char)64);
+  function3(2, 2);
+  function4("12345678c++");
+  function5(0);
+  int t[3];
+  t[2] = 5;
+  function6(t);
+}
